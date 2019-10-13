50b553956bf82ce8f0eec0baa48a300174f37d90
Josué Almeida <josue.almeida@gmail.com>
2019-10-13 01:02:23 +0100

    Added Enemy class

---
+++ src/org/academiadecodigo/stringrays/frontbattle/Enemy.java
@@ -0,0 +1,126 @@
+package org.academiadecodigo.stringrays.frontbattle;
+
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Enemy implements Movables {
+
+    private Position position;
+    private String name;
+    private int health = 100;
+    private boolean destroyed;
+    private Field field;
+    private Direction direction;
+    private Picture healthPicture;
+
+    public Enemy(String name, Position position, Field field, Direction direction, Picture healthPicture) {
+        this.name = name;
+        this.position = position;
+        this.field = field;
+        this.direction = direction;
+        this.healthPicture = healthPicture;
+        healthPicture.draw();
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Bullet attack() {
+
+        //TODO REMOVE THIS CONDITION IF THE GAME OVER IS WELL DONE
+        if (health > 0) {
+
+            //TODO GET THE BULLET SIZE FIXED AND SHOOT RIGHT WITHOUT EXPANDING CANVAS
+            switch (direction) {
+                case UP:
+                    return new Bullet(new Position((position.getX() + position.getMaxX()) / 2, position.getY() - 10, field, "img/bullet.png"), direction);
+                case DOWN:
+                    return new Bullet(new Position((position.getX() + position.getMaxX()) / 2, position.getMaxY(), field, "img/bullet.png"), direction);
+                case LEFT:
+                    return new Bullet(new Position(position.getX() - 10, (position.getY() + position.getMaxY()) / 2, field, "img/bullet.png"), direction);
+                case RIGHT:
+                    if (this.position.getMaxX() < field.getWidth() - 10) {
+                        return new Bullet(new Position(position.getMaxX(), (position.getY() + position.getMaxY()) / 2, field, "img/bullet.png"), direction);
+                    }
+            }
+        }
+        return null;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public void move(Direction direction, int distance) {
+
+        switch (direction) {
+            case UP:
+                position.moveUp(distance);
+                break;
+            case DOWN:
+                position.moveDown(distance);
+                break;
+            case LEFT:
+                position.moveLeft(distance);
+                break;
+            case RIGHT:
+                position.moveRight(distance);
+                break;
+        }
+    }
+
+    public void hit(int damage) {
+        health -= damage;
+
+        switch (health) {
+            case 90:
+                healthPicture.load("img/90health.png");
+                break;
+            case 80:
+                healthPicture.load("img/80health.png");
+                break;
+            case 70:
+                healthPicture.load("img/70health.png");
+                break;
+            case 60:
+                healthPicture.load("img/60health.png");
+                break;
+            case 50:
+                healthPicture.load("img/50health.png");
+                break;
+            case 40:
+                healthPicture.load("img/40health.png");
+                break;
+            case 30:
+                healthPicture.load("img/30health.png");
+                break;
+            case 20:
+                healthPicture.load("img/20health.png");
+                break;
+            case 10:
+                healthPicture.load("img/10health.png");
+                break;
+            case 0:
+                healthPicture.delete();
+        }
+        //position.hide();
+        //position.show();
+        if (health <= 0) {
+            position.hide();
+            destroyed = true;
+        }
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
